faba4569be4ed3cba10eea8ced0194d0875b90af
Kyle Avery <xirdie@gmail.com>
2012-04-08 00:42:11 -0700

    GameObject class implemented and main arguments was fixed.
    Also included code to insure the headers only get called once if we implement inheritance.

--- Terraf0rm/Terraf0rm/assembly.h
+++ Terraf0rm/Terraf0rm/assembly.h
@@ -1,7 +1,7 @@
 /************************************************************************
 *                     Headers go in this header file!!                  *
 *************************************************************************/
-
+#pragma once
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 
