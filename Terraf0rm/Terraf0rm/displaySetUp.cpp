46261088d50822a78d1f09e8916ce52095c45507
Kyle Avery <xirdie@gmail.com>
2012-04-07 19:16:30 -0700

    Added Support for detecting to screen resolution and then setting the game screen to fullscreen with the new screen resolution data.

--- Terraf0rm/Terraf0rm/displaySetUp.cpp
+++ Terraf0rm/Terraf0rm/displaySetUp.cpp
@@ -6,15 +6,29 @@ void displaySet ()
 {
 	//Display Variable
 	ALLEGRO_DISPLAY *display = NULL;
-	//sets the display
-	display = al_create_display(640, 480);
+	//Monitor Info
+	ALLEGRO_MONITOR_INFO info;
+	//Set width and height Variables
+	int w = 0, h = 0;
+	//Get Monitor Info
+	al_get_monitor_info(0, &info);
+	//Gain and set the appropriat x and y valuse
+	w = info.x2 - info.x1;
+	h = info.y2 - info.y1;
+	//Sets the display to fullscreen and the same size as your screen
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	//Sets the default display
+	if (w != 0 && h != 0)
+		display = al_create_display(w, h);
+	else
+		display = al_create_display(800, 600); //sets the back up display
 	//Makes sure the display isn't NULL
 	if(!display)
 	{
 	  al_show_native_message_box(NULL, NULL, NULL, "failed to initialize display!", NULL, NULL);
 	}
 	//Changes the color of the game window
-	al_clear_to_color(al_map_rgb(0,0,255));
+	al_clear_to_color(al_map_rgb(0,255,0));
 	//Sends the changes to the screen
 	al_flip_display();
 	//Pauses the screen for 5 seconds
