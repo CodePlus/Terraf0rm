0db49645576e4d3577a280bc0b722b5446240653
Kyle Avery <xirdie@gmail.com>
2012-04-10 14:27:53 -0700

    Setting up the player Hero Class

---
+++ Terraf0rm/Terraf0rm/Hero.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GameObject.h"
+
+/*******************************************
+*				Player Class			   *
+*******************************************/
+
+class Hero : public GameObject
+{
+	public:
+			Hero();
+			~Hero();
+	private:
+};
